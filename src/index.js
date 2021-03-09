fc94c63d997c1cd34f05950a6997ea0f5ed7182e
Anastasia <anastasia.mitsina@yandex.by>
2021-03-09 16:21:42 +0300

    feat: created functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+exports.min = function min(array) {
+    array = typeof array !== "undefined" ? array : [];
+    return array.length ? Math.min(...array) : 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    array = typeof array !== "undefined" ? array : [];
+    return array.length ? Math.max(...array) : 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    array = typeof array !== "undefined" ? array : [];
+    if (array.length) {
+        let total = array.reduce((sum, num) => sum + num);
+        return total / array.length;
+    } else {
+        return 0;
+    }
+};
